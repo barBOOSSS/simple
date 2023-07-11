0b392f15688e85c74d3b6546d468d7ae40083466
barbo <barboosss@gmail.com>
2023-07-12 00:46:36 +0300

    10

--- src/main/java/by/test/Main.java
+++ src/main/java/by/test/Main.java
@@ -2,9 +2,6 @@ package by.test;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello all peoples!");
-        System.out.println("eu eu");
-        System.out.println("2222222222222222");
-        System.out.println("555555555555");
+        System.out.println("Hello");
     }
 }
